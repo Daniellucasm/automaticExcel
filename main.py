122996210ba51d1d1a8a3e197499d310bdeeb957
daniellucasm <danielmurta123@gmail.com>
2024-06-27 17:59:15 -0300

    Adding information username

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import openpyxl as op
+import os
 
 workbook = op.load_workbook('modelo_rco.xlsx')
 
@@ -14,6 +15,20 @@ vet[0].value = input("Entre com o valor da celula A8: ")
 # Salvar as alterações no arquivo
 workbook.save('modelo_rco.xlsx')
 
+#Coletando o nome do usuario para preenchimento
+username = os.getenv('USERNAME')
+
+
+class Excel:
+    finalidade
+    numRequest
+    numProject
+    name 
+    date
+    user
+    dateCreation
+
+    
 
 #workbook.save('nomes.xlsx') --- Salvar planilha
 #workbook.create_sheet('Teste', 0) --- Criar uma aba nova
